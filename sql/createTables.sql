70ac08e5629ce43fabd6f7caec0710ae9f945271
hboppe <hanna.nascimento31@gmail.com>
2023-04-11 15:00:15 -0400

    feat: add sql commands to createTables.sql file

---
+++ sql/createTables.sql
@@ -0,0 +1,54 @@
+CREATE TYPE os_options AS ENUM ('Windows', 'Linux', 'MacOS');
+
+CREATE TABLE developers (
+	id SERIAL PRIMARY KEY,
+	"name" VARCHAR(50) NOT NULL,
+	email VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE developer_infos(
+	id SERIAL PRIMARY KEY,
+	"developerSince" DATE NOT NULL,
+	"preferredOS" os_options NOT NULL,
+	"developerId" INTEGER UNIQUE NOT NULL, 
+	FOREIGN KEY ("developerId") REFERENCES developers ON DELETE CASCADE
+);
+
+CREATE TABLE projects(
+	id SERIAL PRIMARY KEY,
+	description TEXT,
+	"estimatedTime" VARCHAR(20) NOT NULL,
+	repository VARCHAR(120) NOT NULL,
+	"startDate" DATE NOT NULL,
+	"endDate" DATE,
+	"developerId" INTEGER,
+	FOREIGN KEY ("developerId") REFERENCES developers ON DELETE SET NULL
+);
+
+CREATE TABLE technologies(
+	id SERIAL PRIMARY KEY,
+	"name" VARCHAR(30) NOT NULL
+);
+
+CREATE TABLE projects_technologies(
+	id SERIAL PRIMARY KEY,
+	"addedIn" DATE NOT NULL,
+	"technologyId" INTEGER NOT NULL,
+	"projectId" INTEGER NOT NULL,
+	FOREIGN KEY ("technologyId") REFERENCES technologies ON DELETE CASCADE,
+	FOREIGN KEY ("projectId") REFERENCES projects ON DELETE CASCADE
+);
+
+INSERT INTO 
+	technologies (name)
+VALUES
+	('JavaScript'),
+	('Python'),
+	('React'),
+	('Express.js'),
+	('HTML'),
+	('CSS'),
+	('Django'),
+	('PostgreSQL'),
+	('MongoDB')
+;
